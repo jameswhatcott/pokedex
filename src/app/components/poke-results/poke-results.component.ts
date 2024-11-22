cebb8770b73fe393012e92e04f7117e1d40d5faf
jameswhatcott <james@whatcott.com>
2024-11-22 17:59:19 -0600

    trying to display basic poke info, need more time

--- src/app/components/poke-results/poke-results.component.ts
+++ src/app/components/poke-results/poke-results.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokeService } from '../../service/poke.service';
 
 @Component({
   selector: 'app-poke-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './poke-results.component.html',
-  styleUrl: './poke-results.component.scss'
+  styleUrls: ['./poke-results.component.scss']
 })
-export class PokeResultsComponent {
+export class PokeResultsComponent implements OnInit {
+  pokemon: any;
 
+  constructor(private pokeService: PokeService) { }
+
+  ngOnInit(): void {
+    this.pokeService.getPokemon('pikachu').subscribe(data => {
+      this.pokemon = data;
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PokeResultsComponent } from "../components/poke-results/poke-results.component";
+import { PokeSearchComponent } from '../components/poke-search/poke-search.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, PokeResultsComponent, PokeSearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
