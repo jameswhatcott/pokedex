f2a3727ecca85eeeda2a3d826538832e798bd585
jameswhatcott <james@whatcott.com>
2024-11-22 16:01:27 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header />
+    <main>
+      <router-outlet />
+    </main>
+  `,
+  styles: [`
+    main {
+      padding: 16px;
+    }
+
+  `
+
+  ],
 })
 export class AppComponent {
   title = 'pokedex';
