ff9cf8623875a124d1d61f0661387d5f35cdce4b
jameswhatcott <james@whatcott.com>
2024-12-03 21:51:20 -0600

    added the link in the navbar, adding page details next

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { BrowseComponent } from './components/browse/browse.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+  },
+  { 
+    path: 'browse', 
+    pathMatch: 'full',
+    loadComponent: () => import('./components/browse/browse.component').then(m => m.BrowseComponent),
   },
 ];
