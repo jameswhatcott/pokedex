a4a69b87a3c7151e7204b192347d56c17af75457
jameswhatcott <james@whatcott.com>
2024-12-05 14:02:12 -0600

    got the browse pokemon component to render, don't know entirely how

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeService } from '../../service/poke.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(private pokeService: PokeService) {}
+
+  ngOnInit() {
+    this.fetchPokemons();
+  }
+
+  fetchPokemons() {
+    this.pokeService.getAllPokemon().subscribe(response => {
+      const results = response.results.slice(0, 10); // Limit to 10 Pokémon for display
+      results.forEach((result: any) => {
+        this.pokeService.getPokemon(result.name).subscribe(pokemonData => {
+          this.pokemons.push({
+            name: pokemonData.name,
+            imageUrl: pokemonData.sprites.front_default
+          });
+        });
+      });
+    });
+  }
+
+  showDetails(pokemon: any) {
+    // Logic to show modal with Pokémon details
+    console.log(pokemon);
+  }
+
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CapitalizeFirstPipe } from '../pipes/capitalize-first.pipe'; // Adjust the path as necessary
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
 
 @NgModule({
-  declarations: [CapitalizeFirstPipe],
-  imports: [CommonModule],
-  exports: [CapitalizeFirstPipe]
+  declarations: [ CapitalizeFirstPipe],
+  imports: [
+    CommonModule, 
+    BrowserModule, 
+  ],
+  exports: [CapitalizeFirstPipe],
+  bootstrap: []
 })
 export class SharedModule { }
